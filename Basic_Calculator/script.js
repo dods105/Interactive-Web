33c7ad9a900ed2f080f64e65ba87d8ebe9aaf7de
stacy105 <laranyxer@gmail.com>
2025-07-26 12:38:25 +0800

    first commit

---
+++ Basic_Calculator/script.js
@@ -0,0 +1,22 @@
+const screen = document.querySelector('.screen');
+const buttons = document.querySelectorAll('button');
+
+let calculation = [];
+let accumulativeCalculation;
+
+function calculate (button){
+    const value = button.textContent;
+
+    if(value === 'AC'){
+        calculation = [];
+        screen.textContent = '';
+    }else if(value === '='){
+        screen.textContent = eval(accumulativeCalculation);
+    }else{
+        calculation.push(value);
+        accumulativeCalculation = calculation.join('');
+        screen.textContent = accumulativeCalculation
+    }
+};
+
+buttons.forEach(button => button.addEventListener('click', () => calculate(button)));
---
+++ To Do List/script.js
@@ -0,0 +1,54 @@
+const btn = document.querySelector('button');
+const inputTask = document.querySelector('.text');
+const date = document.querySelector('#date');
+const taskContainer = document.querySelector('.list');
+
+function addTask(button){
+    let value = button.textContent
+    const task  = document.createElement('div')
+    const li = document.createElement('li')
+    const completed = document.createElement('INPUT')
+    completed.setAttribute('type', 'checkbox')
+    const activity = document.createElement('p');
+    const deadline = document.createElement('p');
+    
+    task.classList.add('task')
+
+    let deadlineValue = date.value? date.value : 'None'
+
+    if(value==='submit'){
+        completed.classList.add('checkTask')
+        task.appendChild(completed)
+
+        activity.textContent = inputTask.value
+        activity.style.fontSize = '15px'
+        activity.style.fontWeight = '800'
+        deadline.textContent = `Deadline: ${deadlineValue}`
+        deadline.style.fontSize = '12px'
+        li.append(activity);
+        li.append(deadline)
+        li.style.listStyle = 'none'
+        task.appendChild(li)
+
+
+        if(inputTask.value == ''){
+            alert('Input a task')
+        }else{
+            taskContainer.append(task)
+        }
+
+        inputTask.value = ''
+
+        completed.addEventListener('change', function(){
+            if(completed.checked){
+                task.remove()
+            }
+        })
+        
+    }
+}
+
+btn.addEventListener('click', () => addTask(btn));
+
+
+
